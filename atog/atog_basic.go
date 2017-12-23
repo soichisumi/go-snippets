e2e54fa582f5d00aa469ee3e15d72d185894309c
yoyoyousei <yoyoyousei2016@gmail.com>
2017-12-23 22:57:07 +0900

    sliceあたりまで書いた

--- atog/atog_basic.go
+++ atog/atog_basic.go
@@ -1,5 +1,25 @@
 package atog
 
-func add(x int, y int) int{
-	return x+y
+import "fmt"
+
+var c, python, java bool = true, false, true
+
+type Point2d struct {
+	x, y int
+}
+
+type Point3d struct {
+	x, y, z int
+}
+
+func useVariables() {
+	fmt.Sprintf("%t %t %t", c, python, java)
+}
+
+func addByPointer(p *int, num int) {
+	*p += num
+}
+
+func add(x int, y int) int {
+	return x + y
 }
--- atog/atog_basic_test.go
+++ atog/atog_basic_test.go
@@ -1,26 +1,61 @@
 package atog
 
-import(
+import (
 	"testing"
 	"github.com/yoyoyousei/SandBox/ext"
 	"github.com/yoyoyousei/SandBox/testutil"
 )
 
-func TestGettingExtValue(t *testing.T){
+func TestGettingExtValue(t *testing.T) {
 	actual1 := ext.Pi
-	if !testutil.FloatEqE10(float64(3.141592), actual1){
+	if !testutil.FloatEqE10(float64(3.141592), actual1) {
 		t.Errorf(" %f が返されました", actual1)
 	}
 
 	actual2 := ext.GetPI()
-	if !testutil.FloatEqE10(float64(3.14159265358979), actual2){
+	if !testutil.FloatEqE10(float64(3.14159265358979), actual2) {
 		t.Errorf("%fが返されました", actual2)
 	}
 }
 
-func TestAdd(t *testing.T){
-	actual := add(1,2)
-	if actual != 3{
-		t.Errorf("%dが返されました")
+func TestAdd(t *testing.T) {
+	actual := add(1, 2)
+	if actual != 3 {
+		t.Errorf("%dが返されました", actual)
+	}
+}
+
+func TestAddByPointer(t *testing.T) {
+	var x, y int = 1, 2
+	addByPointer(&x, y)
+	if x != 3 {
+		t.Errorf("%dが返されました", x)
+	}
+}
+
+func TestStruct(t *testing.T) {
+	tmp2d := Point2d{x: 5, y: 10}
+	tmp3d := Point3d{x: 10, y: 1, z: 111}
+
+	actual2d := tmp2d.x
+	actual3d := tmp3d.y
+
+	if actual2d != 5 {
+		t.Errorf("%dが返されました", actual2d)
+	}
+
+	if actual3d != 1 {
+		t.Errorf("%dが返されました", actual3d)
+	}
+}
+
+func TestArrayAndSlice(t *testing.T) {
+	var a = [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	if a[0] != 1 {
+		t.Errorf("err")
+	}
+	s:=a[len(a)/2:] //スライスを作成。スライスは配列への参照
+	if s[0] != 6{
+		t.Errorf("er")
 	}
 }
