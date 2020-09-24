2256582f1f6db6a272133ab38f4eea38df65f64d
Soichi Sumi <soichi.sumi@gmail.com>
2020-09-24 14:17:41 +0900

    add gcp/storage list objects

---
+++ gcp/script/spanner-tablesize-aggregator/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"cloud.google.com/go/storage"
+	"context"
+	"fmt"
+	"github.com/cloudfoundry/bytefmt"
+	"github.com/soichisumi/go-util/logger"
+	"go.uber.org/zap"
+	"google.golang.org/api/iterator"
+	"sort"
+	"strings"
+)
+
+func main() {
+	ctx := context.Background()
+	c, err := storage.NewClient(ctx) // use default application service account
+	if err != nil {
+		logger.Fatal("", zap.Error(err))
+	}
+	b := c.Bucket("BUCKET_NAME")
+	it := b.Objects(ctx, &storage.Query{
+		Prefix: "FOLDER_NAME", // objectName = {folder1}/{folder2}/.../{fileName}
+	})
+
+	sizeMap := make(map[string]int64)
+
+	logger.Info("start listing objects")
+	for {
+		o, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			logger.Error("", zap.Error(err))
+			break
+		}
+		splitted := strings.Split(o.Name, "/")
+		name := splitted[len(splitted)-1]
+
+		var tableName string
+		switch {
+		case strings.Contains(name, "-manifest"):
+			tableName = strings.Split(name, "-manifest")[0]
+		case strings.Contains(name, ".avro"):
+			tableName = strings.Split(name, ".avro")[0]
+		}
+
+		sizeMap[tableName] = sizeMap[tableName] + o.Size
+
+		logger.Info("obj", zap.String("name", o.Name), zap.Int64("size", o.Size))
+	}
+	logger.Info("object listing done.")
+
+	logger.Info("result:")
+
+	keys := make([]string, 0, len(sizeMap))
+	for k := range sizeMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	var total int64
+	for _, k := range keys {
+		fmt.Printf("%s: %s Byte\n", k, bytefmt.ByteSize(uint64(sizeMap[k])))
+		total += sizeMap[k]
+	}
+	logger.Info("done", zap.String("totalSize", bytefmt.ByteSize(uint64(total))))
+}
--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/soichisumi/go-snippets
 go 1.14
 
 require (
-	cloud.google.com/go v0.62.0
+	cloud.google.com/go v0.66.0
 	cloud.google.com/go/storage v1.10.0
 	github.com/GincoInc/jsonrpc v0.0.0-20200410064044-1e3ac997f915
+	github.com/cloudfoundry/bytefmt v0.0.0-20200131002437-cf55d5288a48
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/emirpasic/gods v1.12.0
-	github.com/google/go-cmp v0.5.1
+	github.com/google/go-cmp v0.5.2
 	github.com/google/uuid v1.1.1
 	github.com/gorilla/websocket v1.4.2
 	github.com/jmoiron/sqlx v1.2.0
@@ -16,9 +17,11 @@ require (
 	github.com/newrelic/go-agent/v3 v3.8.1
 	github.com/newrelic/go-agent/v3/integrations/nrmysql v1.2.0
 	github.com/nlopes/slack v0.6.0
-	github.com/soichisumi/go-util v0.0.0-20200119195933-fc16dfa50f48 // indirect
+	github.com/soichisumi/go-util v0.0.0-20200119195933-fc16dfa50f48
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
-	google.golang.org/api v0.29.0
-	google.golang.org/genproto v0.0.0-20200731012542-8145dea6a485
+	go.uber.org/zap v1.13.0
+	golang.org/x/tools v0.0.0-20200923182640-463111b69878 // indirect
+	google.golang.org/api v0.31.0
+	google.golang.org/genproto v0.0.0-20200914193844-75d14daec038
 )
