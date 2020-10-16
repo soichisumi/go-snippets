d2441976a128efaafd328d4d6b74b8117ba4fd5a
Soichi Sumi <soichi.sumi@gmail.com>
2020-10-17 05:32:49 +0900

    add Test_Json_OverrideUnmarshalMethodOfParticularField

--- go/basic/main_test.go
+++ go/basic/main_test.go
@@ -44,6 +44,87 @@ func Test_json(t *testing.T) {
 	}
 }
 
+type User struct {
+	Name string `json:"name"`
+	Email string `json:"email"`
+}
+type NoTagType struct {
+	A string
+	B int64
+}
+// considering given this struct
+type ExternalOrganization struct {
+	Name string `json:"name"`
+	Users []User `json:"users"`
+	Data NoTagType
+}
+
+type Organization struct {
+	ExternalOrganization
+}
+
+func (o *Organization) UnmarshalJSON(b []byte) error {
+	var m struct{
+		*ExternalOrganization
+		Data2 struct {
+			A string `json:"wowCorruptedKeyA"`
+			B int64 `json:"wowwwCuriousKeyB"`
+		}`json:"data"`
+	}
+	if err := json.Unmarshal(b, &m); err != nil {
+		return err
+	}
+	logger.Info("", zap.Any("m", m), zap.Any("o", o))
+	o.Name = m.Name
+	o.Users = m.Users
+	o.Data = NoTagType{
+		A: m.Data2.A,
+		B: m.Data2.B,
+	}
+	return nil
+}
+
+func Test_Json_OverrideUnmarshalMethodOfParticularField(t *testing.T) {
+	tests := []struct {
+		name string
+		in string
+		exp Organization
+	}{
+		{
+			name: "success",
+			in: `{ 
+					"name": "organization name",
+					"users": [{
+						"name": "name",
+						"email":"mail@example.com"
+					}],
+					"data": {"wowCorruptedKeyA": "wiiii", "wowwwCuriousKeyB": 100}
+				}`,
+			exp: Organization{
+				ExternalOrganization: ExternalOrganization{
+					Name: "organization name",
+					Users: []User{{Name: "name", Email: "mail@example.com"}},
+					Data: NoTagType{A: "wiiii", B: 100},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var u Organization
+			if err := json.NewDecoder(strings.NewReader(tt.in)).Decode(&u); err != nil && err != io.EOF {
+				t.Errorf("err: %+v\n", err)
+				return
+			}
+			if !cmp.Equal(tt.exp, u) {
+				t.Errorf("exp: %+v, act: %+v\n", tt.exp, u)
+				return
+			}
+			fmt.Printf("res: %+v\n", u)
+		})
+	}
+}
+
 func Test_SQLx(t *testing.T) {
 	tests := []struct {
 		name string
